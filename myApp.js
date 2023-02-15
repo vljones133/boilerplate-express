ddbdbc19397f867fe19ba68bf3c72562e971dc9b
Valerie Jones <4821024+vljones133@users.noreply.github.com>
2023-02-15 12:04:30 -0600

    fcc exercises stopped working but hackathon project routes suddenly started working

--- myApp.js
+++ myApp.js
@@ -1,39 +1,21 @@
 let express = require('express');
 let app = express();
+require('dotenv').config();
 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
 
+app.use('/public', express.static(__dirname + '/public'));
 
+app.get('/json', (req, res) => {
+  let message = 'Hello json';
+  process.env.MESSAGE_STYLE == 'uppercase'
+    ? (message = message.toUpperCase())
+    : (message = message);
+  res.json({ message: message });
+});
 
+console.log('Hello World');
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
